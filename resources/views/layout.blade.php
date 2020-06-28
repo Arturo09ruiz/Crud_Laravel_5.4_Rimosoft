758d20b4e2dd539c5be984b82d2c50b3fdef4625
Gregory <gregory09ruiz@gmail.com>
2020-06-27 21:32:48 -0400

    Create_Route_Resource_Product-and_views

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD Laravel 5.4</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="page-header text-center"> CRUD Laravel 5.4 </h1>
+                <hr>
+            </div>
+            @yield('content')
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>       
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <div class="col-sm-8">
+        Listado De Productos
+    </div>    
+    <div class="col-sm-4">
+        Mensaje
+    </div>
+@endsection
